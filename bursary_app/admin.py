a0e86fd1623dca9d4932a586391c3144b27704c4
google-boy <otienoh13@gmail.com>
2021-09-06 17:42:57 +0300

    Register first few models in the admin.

--- bursary_app/admin.py
+++ bursary_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import (
+    BursaryCycle, Application, Applicant, Residence
+)
 # Register your models here.
+
+admin.site.register(BursaryCycle)
+admin.site.register(Application)
+admin.site.register(Applicant)
+admin.site.register(Residence)
