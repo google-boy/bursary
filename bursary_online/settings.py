c0db333632a6ac30a268e77e2381450730f8251a
google-boy <otienoh13@gmail.com>
2021-09-19 12:08:16 +0300

    Allow all hosts.

--- bursary_online/settings.py
+++ bursary_online/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-07i!x)8(-%tt=ll-!&o0#(c48(ny8uc$wg08%l8yi-6l5_&b%8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
