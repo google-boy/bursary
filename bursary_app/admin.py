2fe11293d70e298733ce736485426e056446767f
google-boy <otienoh13@gmail.com>
2021-09-28 21:52:49 +0300

    Create a login template.
    Add CustomUser to admin.

--- bursary_app/admin.py
+++ bursary_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import (
+    CustomUser,
     BursaryCycle, Application, Applicant, Residence
 )
 # Register your models here.
-
+admin.site.register(CustomUser)
 admin.site.register(BursaryCycle)
 admin.site.register(Application)
 admin.site.register(Applicant)
