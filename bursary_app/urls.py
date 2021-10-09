d95baf2cf8dd2b7a339d60b703ecaa12c3dd422f
google-boy <otienoh13@gmail.com>
2021-10-09 17:55:02 +0300

    Add applicant login page.

--- bursary_app/urls.py
+++ bursary_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'bursary_app'
 urlpatterns = [
-    path('', views.index, name='index')
-]
+    path('', views.index, name='index'),
+    path('applicant/login', views.LoginView.as_view(), name='login'),
+    ]
--- bursary_app/views.py
+++ bursary_app/views.py
@@ -1,7 +1,12 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth import views as auth_views
+from  django.views.generic.edit import CreateView
+from bursary_app.models import Applicant
 # Create your views here.
 
 def index(request):
-    return render(request, 'bursary_app/index.html')
+    return render(request, 'index.html')
+
+class LoginView(auth_views.LoginView):
+    template_name = 'bursary_app/login.html'
+
