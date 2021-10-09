98a2817fd84aa301488db3be7bd75243364a69d4
google-boy <otienoh13@gmail.com>
2021-10-09 08:20:49 +0300

    Code refactor.

--- bursary_app/admin.py
+++ bursary_app/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import (
     CustomUser,
     BursaryCycle, Application, Applicant, Residence
 )
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, UserAdmin)
 admin.site.register(BursaryCycle)
 admin.site.register(Application)
 admin.site.register(Applicant)
