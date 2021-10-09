60acd27891bd826eafd6848f7537c43586808f45
google-boy <otienoh13@gmail.com>
2021-10-09 17:35:44 +0300

    Create bursary_app forms module.

---
+++ bursary_app/forms.py
@@ -0,0 +1,10 @@
+from django.forms import forms
+
+class ApplicantCreationForm(forms.Form):
+    pass
+
+class CustomUserCreationForm(forms.Form):
+    pass
+
+class CustomUserUpdateform(forms.Form):
+    pass
