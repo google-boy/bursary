0ef6b3aaa11e0fe6d4aec7dba7e2e79e3b861ffb
google-boy <otienoh13@gmail.com>
2021-10-10 09:56:42 +0300

    Add ApplicantAuthenticationForm.

--- bursary_app/forms.py
+++ bursary_app/forms.py
@@ -1,5 +1,10 @@
-from django.forms import forms
+from django.forms import forms, ValidationError
+from django.contrib.auth.forms import(
+    AuthenticationForm,
+)
+from django.utils.translation import gettext_lazy as _
 
+# Bursary app forms
 class ApplicantCreationForm(forms.Form):
     pass
 
@@ -8,3 +13,12 @@ class CustomUserCreationForm(forms.Form):
 
 class CustomUserUpdateform(forms.Form):
     pass
+
+class ApplicantAuthenticationForm(AuthenticationForm):
+    def confirm_login_allowed(self, user) -> None:
+        super().confirm_login_allowed(user)
+        if not user.is_applicant:
+            raise ValidationError(
+                _('Staff accounts are not allowed to login here'),
+                code='no_staff_accounts'
+            )
