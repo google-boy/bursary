05a80498d20e9be939a893cb05f0593ac5d5b470
google-boy <otienoh13@gmail.com>
2021-10-10 11:14:08 +0300

    Restrict view access to authenticated users.

--- bursary_app/views.py
+++ bursary_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import views as auth_views
 from  django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from bursary_app.models import Applicant
 from bursary_app.forms import ApplicantAuthenticationForm
 # Create your views here.
@@ -12,7 +13,7 @@ class LoginView(auth_views.LoginView):
     template_name = 'bursary_app/login.html'
     authentication_form = ApplicantAuthenticationForm
 
-class ApplicantCreateView(CreateView):
+class ApplicantCreateView(LoginRequiredMixin, CreateView):
     model = Applicant
     fields = [
         'birth_cert_number',
