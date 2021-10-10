9a6a6de1065f29fd6a9999a2b590b0d1e0b64327
google-boy <otienoh13@gmail.com>
2021-10-10 10:03:33 +0300

    Add ApplicantCreateView via generic CreateView.
    Add applicant create form template.
    
    Add login authentication form

--- bursary_app/views.py
+++ bursary_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import views as auth_views
 from  django.views.generic.edit import CreateView
 from bursary_app.models import Applicant
+from bursary_app.forms import ApplicantAuthenticationForm
 # Create your views here.
 
 def index(request):
@@ -9,4 +10,17 @@ def index(request):
 
 class LoginView(auth_views.LoginView):
     template_name = 'bursary_app/login.html'
+    authentication_form = ApplicantAuthenticationForm
 
+class ApplicantCreateView(CreateView):
+    model = Applicant
+    fields = [
+        'birth_cert_number',
+        'admission_no',
+        'id_number',
+        'first_name',
+        'last_name',
+        'other_name',
+        'date_of_birth',
+        'gender'
+    ]
