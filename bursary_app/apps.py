2e8a7392a5b3b070013c3f757d7a6101221455fd
google-boy <otienoh13@gmail.com>
2021-10-11 09:38:26 +0300

    Give bursary_app a human-readable name.

--- bursary_app/apps.py
+++ bursary_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BursaryAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bursary_app'
+    verbose_name = 'bursary'
