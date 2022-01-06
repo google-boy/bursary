b81e18534af9b3a90f9bb1cb9426af2e1217c361
google-boy <otienoh13@gmail.com>
2022-01-06 12:52:59 +0300

    Update url patterns

--- bursary_app/urls.py
+++ bursary_app/urls.py
@@ -4,9 +4,10 @@ from bursary_app import views
 app_name = 'bursary_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('applicant/dashboard', views.dashboard, name='dashboard'),
-    path('applicant/register', views.ApplicantRegisterView.as_view(), name='register'),
-    path('applicant/login', views.LoginView.as_view(), name='login'),
-    path('applicant/profile/edit', views.ApplicantCreateView.as_view(), name='edit_profile'),
-    path('applicant/logout', views.LogoutView.as_view(), name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('profile', views.ApplicantProfileView.as_view(), name='profile'),
+    path('register', views.ApplicantRegisterView.as_view(), name='register'),
+    path('login', views.LoginView.as_view(), name='login'),
+    path('profile/edit', views.ApplicantCreateView.as_view(), name='edit_profile'),
+    path('logout', views.LogoutView.as_view(), name='logout'),
 ]
